ba059cdd64a3876275a19a5f4c1b4849cfff533f
Lionda Kingston <lindaknobel@mail.weber.edu>
2024-10-20 11:19:35 -0600

    intial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include "Expense.inc"
+int main() {
+    std::vector<Expense> expenses;
+    std::string action;
 
-int main(){
-    std::cout << "Hello world" << std::endl;
+    while (true) {
+        std::cout << "Pick an action (add, total, category, quit):  ";
+        std::cin >> action;
+
+        if(action =="add") {
+            float amount;
+            std::string category, date;
+            std::cout << "Enter amount: ";
+            std::cin >> amount;
+            std::cout << "Enter category: ";
+            std::cin >> category;
+            std::cout << "Enter date( MM-DD-YYYY): ";
+            std::in >> date;
+
+            Expense newExpense(amount, category, date);
+            addExpense(expenses, newExpense);
+        } else if (action ==  "total") {
+            std::cout << "Total expenses: $" << calculateTotal(expenses) << std::endl;
+        } else if (action == "category") {
+            std::string category;
+            std::cout << "Enter categoryto filter: ";
+            std::cin >> category;
+            displayExpensesByCategory(expenss, category);
+        } else if (action == "quit") {
+            break;
+        } else if {
+            std::cout << "Not Vaild, pick again." << std::endl;
+        }
+    }
     return 0;
 }
